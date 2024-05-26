b32345c0703a61dfe4d53da02963c06b3889fc71
adham mohamed <adham.mohamed@ELSEWEDY-SCHOOL.LOCAL>
2024-05-26 12:50:45 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,30 @@
+alert("h e l l o ")
+function img11()
+{
+    alert("you are in the img1")
+}
+function img22()
+{
+    alert("you are in the img2")
+}
+function img33()
+{
+    alert("you are in the img3")
+}
+function img44()
+{   
+    alert("you are in the img4")
+}
+function img55()
+{
+    alert("you are in the img5")
+}
+function click()
+{
+    window.display = none;
+}
+function nextbtn()
+{
+    alert("bal bla bla");
+    document.location.href('https://github.com/');
+}
